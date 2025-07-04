f64ceaab096dc02e266f93bd0f4d8e61dff8fa5f
santiaguero7 <aguero17.2001@gmail.com>
2025-07-04 00:42:28 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
   }, []);
 
   return (
-    <div>
+    <div className="overflow-x-hidden bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800">
       <Navigation />
       <Hero />
       <About />
